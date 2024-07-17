1b913066daf71fb6f825448c64a6466f9253f35e
Saima223 <siddiqui22062004@gmail.com>
2024-07-17 23:55:19 +0530

    Start by learning TypeScript basics

---
+++ TypeScriptt/variables.js
@@ -0,0 +1,7 @@
+var found = true;
+var grade = 96.4;
+var fname = "Saima";
+var lname = "siddiqui";
+console.log(found);
+console.log("Grade is " + grade);
+console.log("Hey" + fname + " " + lname + "!");
---
+++ TypeScriptt/variables.ts
@@ -0,0 +1,8 @@
+let found: boolean = true;
+let grade: number = 96.4;
+let fname: String = "Saima";
+let lname: String = "siddiqui";
+
+console.log(found);
+console.log("Grade is " + grade);
+console.log("Hey" + fname + " " + lname + "!")
