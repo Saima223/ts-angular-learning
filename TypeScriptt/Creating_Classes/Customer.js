34e91f3d0f1e5f3456a4ee75c05d8019ef5efc88
Saima223 <siddiqui22062004@gmail.com>
2024-07-20 21:18:48 +0530

    Implementation of the constructor.

--- TypeScriptt/Creating_Classes/Customer.js
+++ TypeScriptt/Creating_Classes/Customer.js
@@ -1,10 +1,10 @@
 var Customer = /** @class */ (function () {
-    function Customer() {
+    function Customer(theFirst, theLast) {
+        this.firstName = theFirst;
+        this.lastName = theLast;
     }
     return Customer;
 }());
-var myCustomer = new Customer();
-myCustomer.firstName = "Joh";
-myCustomer.lastName = "Doe";
+var myCustomer = new Customer("Jinni", "jammi");
 console.log(myCustomer.firstName);
 console.log(myCustomer.lastName);
--- TypeScriptt/Creating_Classes/Customer.ts
+++ TypeScriptt/Creating_Classes/Customer.ts
@@ -1,12 +1,15 @@
 class Customer{
     firstName : string;
     lastName : string;
+
+    constructor(theFirst: string, theLast: string){
+        this.firstName = theFirst;
+        this.lastName = theLast;
+    }
 }
 
-let myCustomer = new Customer();
+let myCustomer = new Customer("Jinni", "jammi");
 
-myCustomer.firstName = "Joh";
-myCustomer.lastName = "Doe";
 
 console.log(myCustomer.firstName);
 console.log(myCustomer.lastName);
