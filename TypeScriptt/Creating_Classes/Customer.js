579a36521f34fffdcc2c5e95e7e1327fc6353c05
Saima223 <siddiqui22062004@gmail.com>
2024-07-20 19:22:00 +0530

    Definition of a Class.

---
+++ TypeScriptt/Creating_Classes/Customer.js
@@ -0,0 +1,10 @@
+var Customer = /** @class */ (function () {
+    function Customer() {
+    }
+    return Customer;
+}());
+var myCustomer = new Customer();
+myCustomer.firstName = "Joh";
+myCustomer.lastName = "Doe";
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
---
+++ TypeScriptt/Creating_Classes/Customer.ts
@@ -0,0 +1,12 @@
+class Customer{
+    firstName : string;
+    lastName : string;
+}
+
+let myCustomer = new Customer();
+
+myCustomer.firstName = "Joh";
+myCustomer.lastName = "Doe";
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
